3a5b33b8dc712dfcb3ed99675dc93fe45a1ab6fc
Jack <ixenroh@gmail.com>
2021-11-14 13:29:00 +0100

    Returned paths to be relative to run_pipeline.py in cleaner.py and updated it with new arguments to make testing easier

--- scripts/cleaner.py
+++ scripts/cleaner.py
@@ -166,7 +166,7 @@ def create_correlation_graphs(correlations, fig_location, threshold):
     return subgraphs
 
 
-def save_mask(df, mask_name, columns: [None, list] = None, rows: [None, list] = None, data_dir='../data'):
+def save_mask(df, mask_name, columns: [None, list] = None, rows: [None, list] = None, data_dir='data'):
     if rows is None and columns is not None:
         mask = (df == df) & (~df.columns.isin(columns))
     elif rows is not None and columns is None:
@@ -180,8 +180,10 @@ def save_mask(df, mask_name, columns: [None, list] = None, rows: [None, list] = 
     mask.to_csv(mask_path, index=False)
 
 
-def check_correlated_column(df, threshold=0.9, remove=False, preserve_columns=[], plot_dir='../plot',
-                            data_dir='../data') -> pd.DataFrame:
+def check_correlated_column(df, threshold=0.9, remove=False, preserve_columns=None, plot_dir='plot',
+                            data_dir='data') -> pd.DataFrame:
+    if preserve_columns is None:
+        preserve_columns = []
     graph_location = os.path.join(plot_dir, f'correlations_grapgs_{threshold}.jpeg')
     heatmap_location = os.path.join(plot_dir, f'correlations_heatmap_{threshold}.jpeg')
 
@@ -232,8 +234,8 @@ def check_outliers(df, threshold=4.2, remove=False):
     return df
 
 
-def main(input_file='../data/chem_output/chem_populated.csv', output_file='../data/final/phototox.csv',
-         project_dir='../', correlation_threshold=0.95, outliers_threshold=4.2, preserve_columns=None,
+def main(input_file='data/chem_output/chem_populated.csv', output_file='data/final/phototox.csv',
+         project_dir='.', correlation_threshold=0.95, outliers_threshold=4.2, preserve_columns=None,
          remove=False):
     if preserve_columns is None:
         preserve_columns = []
@@ -253,7 +255,7 @@ def main(input_file='../data/chem_output/chem_populated.csv', output_file='../da
     df.to_csv(output_file, index=False)
 
 
-def process_config(config_file='../conf/cleaner.ini') -> dict:
+def process_config(config_file='conf/cleaner.ini') -> dict:
     """
     Process input config and extract
     :param config_file: config file name location
